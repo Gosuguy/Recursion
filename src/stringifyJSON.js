84f6a256658dbd33f498fd70223ff8e785819dbb
Gosuguy <36428321+Gosuguy@users.noreply.github.com>
2020-01-10 00:51:19 -0500

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,1 +1,43 @@
+// Easy mode:
+// var stringifyJSON = JSON.stringify;
 
+// hard mode:
+var stringifyJSON = function(obj) {
+  var string = "";
+
+  if (typeof(obj) == "number" || typeof(obj) == "boolean") {
+    string += obj.toString();
+
+  } else if (Array.isArray(obj)) {
+    string += '[';
+    _.each(obj, function(element, index) {
+      string += stringifyJSON(element);
+      if (index !== obj.length - 1) {
+        string += ',';
+      }
+    });
+    string += ']';
+
+  } else if (typeof(obj) == "string") {
+    string += '"' + obj + '"';
+
+  } else if (typeof(obj) == "undefined" || obj == null) {
+    string += "null"
+
+  } else if (typeof(obj) == "object") {
+    string += '{';
+    for (var key in obj) {
+      if (typeof(key) != "function" && typeof(key) != "undefined") {
+        string += stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+        if (obj[key] !== Object.keys(obj)[Object.keys(obj).length - 1]) {
+          string += ',';
+        }
+      }
+    }
+    string += '}';
+  } else if (typeof(obj) == "function") {
+    string += '';
+  }
+
+  return string
+};
