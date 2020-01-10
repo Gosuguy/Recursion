c18d32f77eae95ca61790d813a61eaccad375ca3
Gosuguy <36428321+Gosuguy@users.noreply.github.com>
2020-01-10 00:20:08 -0500

    Complete getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,1 +1,19 @@
+// Easy version:
+// var getElementsByClassName = function (className) {
+//   return document.getElementsByClassName(className);
+// };
 
+// Hard version:
+const getElementsByClassName = function(className, element) {
+  var element = element || document.body;
+  let elementList = [];
+  if (element.classList) {
+  	if (element.classList.contains(className)) {
+      elementList.push(element);
+    }
+  }
+  element.childNodes.forEach(function(childNode) {
+    elementList = elementList.concat(getElementsByClassName(className, childNode));
+  });
+  return elementList;
+};
