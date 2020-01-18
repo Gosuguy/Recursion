a19ae08403fded828b31f9c477341a67ee135252
Gosuguy <36428321+Gosuguy@users.noreply.github.com>
2020-01-18 05:54:33 -0500

    Create Second Version To Compare Pros and Cons

--- src/webCrawler.js
+++ src/webCrawler.js
@@ -39,4 +39,53 @@ const getHyperlinks = (htmlObject) => {
   return currentlyCrawling.getElementsByTagName('a');
 }
 
+/*
 webCrawler("https://www.msn.com/", console.log, 1);
+const webCrawler = function (url, depth = 0,  configuration = 0){
+// configuration
+/*
+Tasks:
+Accept a URL to begin its crawl
+Recursively follow links
+Output the URL of crawled pages
+Accept an optional configuration object as an argument that will effect the default behavior of the crawler. Consider configuring:
+The ability to use getElementsByClassName on any of the pages you visit
+The ability to output other kinds of information about the page such as number of script tags, distinct attributes, links to external sites, etc.
+The option to crawl breadth first instead of depth first
+Limit the depth or breadth of the crawl
+Set a revisit or politeness policy
+ Refactor the crawler to use web workers 
+*/
+  let urlList = [url];
+  let results = {0: urlList}
+  if (configuration === 1) {
+  	let scriptTagList = [];
+    results[1] = scriptTagList;
+  }
+  const crawl = (url, depth,  configuration) => {
+    if (depth > 0) {
+      $.get(url)
+      .then((data) => {
+      let hyperlinks = getHyperlinks(data);
+        for(let i = 0; i < hyperlinks.length; i++) {
+          urlList.push(hyperlinks[i].href);
+          crawl(hyperlinks[i].href, depth - 1);
+        }
+          return;
+      });
+    } else { 
+      return;
+    }
+  }
+	crawl(url, depth,  configuration);
+  return urlList;
+}
+
+const getHyperlinks = (htmlObject) => {
+  let currentlyCrawling = document.createElement('div');
+  currentlyCrawling.innerHTML = htmlObject;
+  return currentlyCrawling.getElementsByTagName('a');
+}
+
+console.log('RESULT ', webCrawler("https://www.msn.com/", 1));
+*/
